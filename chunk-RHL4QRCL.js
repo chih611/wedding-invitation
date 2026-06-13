83052d1d7ab1c1447e64c338cb3e591b055f2a2a
chih <chihao.thai@gmail.com>
2026-06-13 12:14:42 +0700

    Auto-generated commit

--- chunk-RHL4QRCL.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-DFYORCTZ.js";import"./chunk-YFQOCWCJ.js";export{a as MainComponent};
---
+++ chunk-WWM7GVAG.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-U2JPX6PZ.js";import"./chunk-YFQOCWCJ.js";export{a as MainComponent};
